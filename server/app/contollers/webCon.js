4e00eb7b17baba6a11ac83793a1963891141b27c
TheRayhan009 <rayhanthestorymaker@gmail.com>
2025-03-22 07:02:11 +0600

    adding insurt , delete, update route.

--- server/app/contollers/webCon.js
+++ server/app/contollers/webCon.js
@@ -13,10 +13,39 @@ let insurt = async(req,res)=>{
     })
 
     data.save();
+    res.send("User Saved in Database");
 }
 
 let del = async(req,res)=>{
-    
+    let UserPhoneNumber= req.body.Unumber ;
+    let findUserData = await modelY.findOne({Pnumber:UserPhoneNumber});
+
+    if(findUserData){
+        let detUserData= await modelY.deleteOne({Pnumber:UserPhoneNumber});
+
+        res.send("User Deleted");
+    }
+    else{
+        res.send("User Not Found");
+    }
 } 
 
-module.exports={xx,insurt};
+let edit = async(req,res)=>{
+    // let OldUserPhoneNumber= req.body.OldPnumber;
+
+    let { OldPnumber, Gname, Gage, GPnumber } = req.body;
+
+    let findUserData = await modelY.findOne({Pnumber:OldPnumber});
+
+    if(findUserData){
+        let UPDUserData= await modelY.findOneAndUpdate({Pnumber:OldPnumber} ,{$set: { name: Gname, age: Gage, Pnumber: GPnumber }} ,{new:true} );
+
+        res.send("User Updated");
+    }
+    else{
+        res.send("User Not Found");
+    }
+
+}
+
+module.exports={xx,insurt,del,edit};
--- server/app/routes/web/route.js
+++ server/app/routes/web/route.js
@@ -1,9 +1,12 @@
 let express = require("express");
-const { xx, insurt } = require("../../contollers/webCon");
+const { xx, insurt, edit ,del} = require("../../contollers/webCon");
 
 let rout = express.Router();
 
 rout.get("/",xx)
 rout.post('/insurt',insurt)
+rout.post('/del',del)
+rout.post('/edit',edit)
+
 
 module.exports=rout;
